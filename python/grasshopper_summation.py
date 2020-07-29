66b6510b486baf1af8faafafaa4d89df4703130a
Hoang Hai Dang <hoanghaidang1998@gmail.com>
2020-07-29 11:29:34 +0700

    Grasshopper - Summation

---
+++ python/grasshopper_summation.py
@@ -0,0 +1,14 @@
+"""
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+summation(2) -> 3
+1 + 2
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+"""
+
+
+def summation(num):
+    return 1 if num < 2 else num + summation(num - 1)
