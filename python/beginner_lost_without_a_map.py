8fa438f2de5399fc8b6947ce73179f633d966a06
danghh-1998 <hoanghaidang@gapo.com.vn>
2021-11-12 15:05:33 +0700

    Beginner - Lost Without a Map

---
+++ python/beginner_lost_without_a_map.py
@@ -0,0 +1,11 @@
+"""
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+"""
+
+
+def maps(a):
+    return list(map(lambda item: item * 2, a))
